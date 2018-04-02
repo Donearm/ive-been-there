9ceed4cf7c3a39ff931184b802037e7d0470057f
Gianluca Fiore <forod.g@gmail.com>
2018-04-02 10:18:31 +0200

    refactoring to use const

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const _ = require('lodash')
 
 const countries = require('./countries.json')
 
+const visitedCountries = _.filter(countries, {"visited": true})
+const livedInCountries = _.filter(countries, {"lived": true})
+const airportCountries = _.filter(countries, {"airport": true})
+const wantToGoCountries = _.filter(countries, {"want-to-go": true})
+
 console.log('')
-console.log('Countries visited: ', _.filter(countries, {visited: true}).length)
-console.log('Lived in: ', _.filter(countries, {lived: true}).length)
-console.log('Passed through airport: ', _.filter(countries, {"airport": true}).length)
-console.log('Wants to visit someday: ', _.filter(countries, {"want-to-go": true}).length)
+console.log('Countries visited: ', visitedCountries.length)
+console.log('Lived in: ', livedInCountries.length)
+console.log('Passed through airport: ', airportCountries.length)
+console.log('Wants to visit someday: ', wantToGoCountries.length)
