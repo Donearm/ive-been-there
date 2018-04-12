efaf80031bb7416ba5897108bcd783f323f66cdd
Gianluca Fiore <forod.g@gmail.com>
2018-04-12 20:20:27 +0200

    colorizing output with chalk

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const chalk = require('chalk');
 
 const countries = require('./countries.json');
 
@@ -8,17 +9,17 @@ const airportCountries = _.filter(countries, {"airport": true});
 const wantToGoCountries = _.filter(countries, {"want-to-go": true});
 
 console.log('');
-console.log('Countries visited: ', visitedCountries.length);
-console.log('Lived in: ', livedInCountries.length);
-console.log('Passed through airport: ', airportCountries.length);
-console.log('Wants to visit someday: ', wantToGoCountries.length);
+console.log('Countries visited: ', chalk.blueBright.bold(visitedCountries.length));
+console.log('Lived in: ', chalk.yellowBright.bold(livedInCountries.length));
+console.log('Passed through airport: ', chalk.cyanBright.bold(airportCountries.length));
+console.log('Wants to visit someday: ', chalk.redBright.bold(wantToGoCountries.length));
 
 if (process.argv[2] === "-v") {
 	// Verbose mode
 	console.log('--------------------------------------');
 	if (visitedCountries.length > 0) {
 		console.log('');
-		console.log("I've been to:");
+		console.log(chalk.whiteBright.bgBlueBright("I've been to:"));
 		for (i = 0; i < visitedCountries.length; i++) {
 			console.log(visitedCountries[i].name);
 		}
@@ -26,7 +27,7 @@ if (process.argv[2] === "-v") {
 
 	if (livedInCountries.length > 0) {
 		console.log('');
-		console.log("I've lived in:");
+		console.log(chalk.black.bgYellowBright("I've lived in:"));
 		for (i = 0; i < livedInCountries.length; i++) {
 			console.log(livedInCountries[i].name);
 		}
@@ -34,7 +35,7 @@ if (process.argv[2] === "-v") {
 
 	if (airportCountries.length > 0) {
 		console.log('');
-		console.log("I've just passed through one of the airports of:");
+		console.log(chalk.black.bgCyanBright("I've just passed through one of the airports of:"));
 		for (i = 0; i < airportCountries.length; i++) {
 			console.log(airportCountries[i].name);
 		}
@@ -42,7 +43,7 @@ if (process.argv[2] === "-v") {
 
 	if (wantToGoCountries.length > 0) {
 		console.log('');
-		console.log("And I still want to go to:");
+		console.log(chalk.whiteBright.bgRed("And I still want to go to:"));
 		for (i = 0; i < wantToGoCountries.length; i++) {
 			console.log(wantToGoCountries[i].name);
 		}
